6ccea46a8696bdf1e2a478419d17fba48e057b25
sudo-style <87545795+sudo-style@users.noreply.github.com>
2023-05-07 10:46:05 -0400

    Update GradientDescent function
    I segregated the gradient descent for each variable, slope and intercept. I was thinking that If I change them one by one they would be out of sync, but I was wrong.
    
    Might want to update this later, the code looks a little messy, but it is very general, might be a few things that I can change inorder to make it even more, but I think that I have fully developed on the concepts and know in and out of how it works.
    
    https://www.youtube.com/watch?v=sDv4f4s2SB8

--- GradientDescent.py
+++ GradientDescent.py
@@ -18,9 +18,15 @@ class Line:
 
         self.previousStepSizeIntercept = 0.0
         self.previousStepSizeSlope = 1.0
-        self.MIN_STEP_SIZE = 0.0001
+        self.MIN_STEP_SIZE = 0.00001
         self.LEARNING_RATE = 0.01
-        self.MAX_RECURSIONS = 1000
+        self.MAX_RECURSIONS = 950
+
+        # some other functions I need apparently
+        self.stepsizeSlope = 1.0
+        self.stepsizeIntercept = 1.0
+        self.total = 0
+
 
     # predicts the height given the the known weight
     def predictedHeight(self, weight):
@@ -40,46 +46,53 @@ class Line:
         print(f"step size Slope:{self.previousStepSizeSlope}\n")
         print()
 
+
+    # these functions are too similar
+    def interceptStep(self):
+        # take the gradient of the loss function
+        dirivitiveIntercept = sp.diff(self.total, self.dintercept)     
+        # this slope tells in what direction and by how much I can step
+        # plug the parameter values into the derivatives/gradient
+        slopeOfIntercept = dirivitiveIntercept.subs(self.subsituitutions)
+        # calculate the step sizes
+        self.stepsizeIntercept = slopeOfIntercept * self.LEARNING_RATE
+        self.intercept = self.intercept - self.stepsizeIntercept
+
+    def slopeStep(self):
+        # take the gradient of the loss function        
+        dirivitiveSlope = sp.diff(self.total, self.dslope)
+        # this slope tells in what direction and by how much I can step
+        # plug the parameter values into the derivatives/gradient
+        slopeOfSlope = dirivitiveSlope.subs(self.subsituitutions)
+        # calculate the step sizes
+        self.stepsizeSlope = slopeOfSlope * self.LEARNING_RATE        
+        self.slope = self.slope - self.stepsizeSlope
+
+    # this adds up all of the functions in terms of variables
+    def sumVars(self):
+        self.total = 0
+        self.dintercept,self.dslope = sp.symbols('intercept slope')
+        for weight,height in self.data:
+            self.total += (height - (self.dintercept + (self.dslope * weight)))**2
+        self.subsituitutions = {self.dintercept: self.intercept, self.dslope: self.slope}
+
     # my question is, should each of these be their own function for one step? I think so, for one step yes
     def gradientDescent(self, totalRecursions = 0):
         if (totalRecursions > self.MAX_RECURSIONS):
             print("MAX_RECURSIONS")
             return
 
-        total = 0
-        intercept,slope = sp.symbols('intercept slope')
-        for weight,height in self.data:
-            total += (height - (intercept + (slope * weight)))**2
-                
-        # take the gradient of the loss function
-        dirivitiveIntercept = sp.diff(total, intercept)#= self.derivitiveOfSSRIntercept()
-        dirivitiveSlope = sp.diff(total, slope)
-        
-        # this slope tells in what direction and by how much I can step
-        # plug the parameter values into the derivatives/gradient
-        subsituitutions = {intercept: self.intercept, slope: self.slope}
-        slopeOfIntercept = dirivitiveIntercept.subs(subsituitutions)
-        slopeOfSlope = dirivitiveSlope.subs(subsituitutions)
-
-        # calculate the step sizes
-        stepsizeIntercept = slopeOfIntercept * self.LEARNING_RATE
-        stepsizeSlope = slopeOfSlope * self.LEARNING_RATE
-        
-
-        self.previousStepSizeIntercept = stepsizeIntercept
-        self.previousStepSizeSlope = stepsizeSlope
-        
-        # calculate the new parameters
-        self.intercept = self.intercept - stepsizeIntercept
-        self.slope = self.slope - stepsizeSlope
-        
-        if (abs(stepsizeSlope) < self.MIN_STEP_SIZE and abs (stepsizeIntercept) < self.MIN_STEP_SIZE):
+        if (abs(self.stepsizeSlope) < self.MIN_STEP_SIZE and abs (self.stepsizeIntercept) < self.MIN_STEP_SIZE):
             print("MINIMUM STEP SIZE")
             return 
+
+        self.sumVars() 
+        self.interceptStep()
+        self.slopeStep()
         
         # recurse through adding one to the max calls
         self.gradientDescent(totalRecursions + 1)
-
+        #print(totalRecursions)
         
 if __name__ == "__main__":
     main()

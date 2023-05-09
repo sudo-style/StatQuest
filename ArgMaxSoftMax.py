1a49afa98256addb6a3bf40843b689889654ad70
sudo-style <87545795+sudo-style@users.noreply.github.com>
2023-05-09 12:29:21 -0400

    Update ArgMaxSoftMax.py
    Just changed the name == main so that when I import this from a function, it wont run

--- ArgMaxSoftMax.py
+++ ArgMaxSoftMax.py
@@ -1,3 +1,5 @@
+# https://youtu.be/KpKog-L9veg?t=520
+# StatQuest ArgMax/SoftMax part 5 Neural Networks
 import math
 
 # sets the max value to 1, all others are zero
@@ -9,5 +11,9 @@ def softMax(*args):
     denominator = sum([math.pow(math.e,x) for x in args])
     return [math.pow(math.e,x)/denominator for x in args]
 
-print(f"softmax: {softMax(1.43, -.4, 0.23)}")
-print(f"argmax: {argMax(1.43, -.4, 0.23)}")
+def main():    
+    print(f"softmax: {softMax(1.43, -.4, 0.23)}")
+    print(f"argmax: {argMax(1.43, -.4, 0.23)}")
+
+if __name__ == "__main__":
+    main()

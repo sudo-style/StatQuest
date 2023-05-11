7d845773b4bf5cbe77c05496b1fa6e078e4f7512
sudo-style <87545795+sudo-style@users.noreply.github.com>
2023-05-10 22:25:25 -0400

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,10 +1,29 @@
 import numpy as np
 import math
+from ActivationFunctions import softPlus
+from ActivationFunctions import relu
 
 def main():
-    nn = NeuralNetwork(2,3,'relu',2) # this will only do random values
-    print(nn.output(0.3,2.3))
+    nnDosage = NeuralNetwork(1,1,'sp',2)
+    nnDosage.biases = [2.14, 1.29, -0.58] # using determined biases and weights to make sure they match the target
+    nnDosage.weights = [-34.4, -2.52, -1.3, 2.28]
+    print(nnDosage.output(0.5))
+    print("target: [1.03]\n")
     
+    nnFlowers = NeuralNetwork(2,3,'relu',2)
+    nnFlowers.weights = [-2.5, -1.5, 0.6, 0.4, -.1, 2.4, -2.2, 1.5, -5.2, 3.7]
+    nnFlowers.biases = [1.6,0.7,0,0,1]
+    print(nnFlowers.output(0.5,.37))
+    print("target: [0.09, 0.86, 0.10]\n")
+
+    nnRandom = NeuralNetwork(1,1,'sp',4,3,4)    
+    print(f"weights: {nnRandom.weights}")
+    print(f"numWeights: {len(nnRandom.weights)}")
+
+    
+    print(f"biases: {nnRandom.biases}")
+    print(f"numWeights: {len(nnRandom.biases)}")
+    print(f"randomOutput: {nnRandom.output(.43)}")
 
 class NeuralNetwork(): # makes a neural network with random values populated, needs to be trained
     def __init__(self, inputs, outputs, funcType, *hiddenLayers):
@@ -13,57 +32,55 @@ class NeuralNetwork(): # makes a neural network with random values populated, ne
         self.funcType = funcType
         self.hiddenLayers = hiddenLayers
         self.fullNetwork = [self.inputs] + list(self.hiddenLayers) + [self.outputs]
-        
-        self.biases = startBiases(self.calcNumBiases())
-        self.weights = randomWeights(self.calcNumWeights())
+        self.biases = [0.0] * self.calcNumBiases() # bias values start at zero
+        self.weights = np.random.rand(self.calcNumWeights()) # give random values for weights
 
-        '''main():
-        nn = nn = NeuralNetwork(2,3,'relu',2)
-        print(nn.output(0.5,.37))
-        self.weights = [-2.5, -1.5, 0.6, 0.4, -.1, 2.4, -2.2, 1.5, -5.2, 3.7]
-        self.biases = [1.6,0.7,0,0,1]
-        # should output this [0.08979999999999991, 0.8632000000000003, 0.10419999999999963]'''
-        
-        '''main():
-        nn = NeuralNetwork(1,2,'sp',2)
-        print(nn.output(0.5))
-        self.biases = [2.14, 1.29, -0.58]
-        self.weights = [-34.4, -2.52, -1.3, 2.28]
-        should output this [1.03]'''
+        self.weights = [0.34523800, 0.63133228, 0.75229619, 0.78843551, 0.31813906, 0.42814637,
+                        0.84948657, 0.12426702, 0.89754243, 0.74264793, 0.18185164, 0.67522291,
+                        0.77942494, 0.26823629, 0.96129143, 0.05732444, 0.28585249, 0.30001379,
+                        0.73170743, 0.46273547, 0.88589686, 0.71097702, 0.51699631, 0.60683042,
+                        0.33108003, 0.92519579, 0.83285673, 0.28336363, 0.61487147, 0.55821392,
+                        0.707886,   0.71905211]
+        self.biases = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        # should be 8.65 ish
 
-    def calcNumBiases(self):
-        return sum(self.hiddenLayers) + self.outputs
+    def calcNumBiases(self): # hidden layers + outputs = numBiases 
+        return sum(self.fullNetwork[1:])
+    
+    '''
+        # multiplies adjcent numbers and sums them
+        2 input, [2,3] hidden, 2 output
+        = 2*2 + 2*3 + 3*2 = 16
+        return 16
+    '''
+    def calcNumWeights(self): 
+        fullNetwork = self.fullNetwork 
+        return sum([x * y for x, y in zip(fullNetwork, fullNetwork[1:])])
 
-    def calcNumWeights(self):
-        fullNetwork = self.fullNetwork
-        total = 0
-        for i in range(len(fullNetwork)-1):
-            total += fullNetwork[i] * fullNetwork[i+1]
-        return total
-
+    # picks the activation function that is requested
     def activationFunction(self, x):
         if self.funcType == 'relu': return relu(x)
         if self.funcType == 'sp': return softPlus(x)
         return None
-    
+
+    def inputToHidden(self,inputs):
+        layers = 1
+        shiftWeight = self.shiftWeight(layers)   
+        shiftBias = self.shiftBias(layers)       
+        fullNetwork = self.fullNetwork      
+        if len(inputs) != fullNetwork[0]: return None
+        sums = self.biases
+        for i in range(self.layers(layers-1)): 
+            sums[shiftBias + i%self.fullNetwork[layers]] += inputs[int(i/fullNetwork[layers])]*self.weights[shiftWeight+i]
+        return sums
+
+    # the reason these have to be different is because the input changes the sums
     def hiddenToHidden(self, sums, layers): 
         shiftWeight = self.shiftWeight(layers) # I don't want any magical constants, except for testing
         shiftBias = self.shiftBias(layers)
         fullNetwork = self.fullNetwork 
-        n = 0
-        for i in range(self.layers(layers-1)): # layers -1 is the previous layer
-            sums[shiftBias+i%fullNetwork[layers]] += self.activationFunction(sums[int(n/fullNetwork[layers])]) * self.weights[shiftWeight+i]
-            n+=1
-        return sums
-
-    def inputToHidden(self,inputs):
-        fullNetwork = self.fullNetwork
-        if len(inputs) != fullNetwork[0]: return None
-        sums = self.biases
-        n = 0
-        for i in range(self.layers(0)):
-            sums[i%2] += inputs[int(n/fullNetwork[1])]*self.weights[i]
-            n += 1
+        for i in range(self.layers(layers-1)): # layers -1 means the previous layer
+            sums[shiftBias+i%fullNetwork[layers]] += self.activationFunction(sums[int(i/fullNetwork[layers])]) * self.weights[shiftWeight+i]
         return sums
 
     def layers(self,x):
@@ -85,7 +102,8 @@ class NeuralNetwork(): # makes a neural network with random values populated, ne
         return sum(multipliedNetwork[0:layers])
 
     def output(self, *args):
-        inputs = args        
+        inputs = args
+        print(inputs)
         outputs = self.outputs
         fullNetwork = self.fullNetwork
         if len(inputs) != fullNetwork[0]: return None
@@ -93,20 +111,11 @@ class NeuralNetwork(): # makes a neural network with random values populated, ne
         # do a summation on the weights, biases of the first input layer to hidden layer
         sums = self.inputToHidden(inputs)
         # hidden to hidden takes care of all of the layers that need an activation layer including the outputs
-        sums = self.hiddenToHidden(sums,2)
-        return sums[-1 * outputs:]
-    
-def startBiases(n):
-    return [0] * n
-
-def randomWeights(n):
-    return np.random.rand(n)
-
-def relu(x):
-    return max(0,x)
-
-def softPlus(x):
-    return math.log(1+math.pow(math.e,x))    
+        print(sums)
+        for i in range(len(self.hiddenLayers)): 
+            sums = self.hiddenToHidden(sums,i+2) # 1 is the first layer which was already added i is zero on first pass
+            print(sums)
+        return sums[-1 * outputs:] #the end of sums are the outputs, if there are outputs then sum[x,x,x,x,a,b,c] a,b,c is the outputs
 
 if __name__ == "__main__":
     main()
